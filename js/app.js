f9e3e35c0e34fb466d3d7053601d5745cf8da14f
Qiming Weng <Qiming@This-is-a-MacBook-Pro.local>
2015-05-05 21:25:10 -0400

    Update

--- js/app.js
+++ js/app.js
@@ -3,25 +3,34 @@ import PopoverControl from "react-popover-control";
 
 const mountNode = document.getElementById("react-app");
 const App = React.createClass({
-	propTypes: {
-
-	},
-	componentDidMount() {
-
-	},
-	componentDidUpdate() {
-
-	},
-	componentWillUnmount() {
-		
-	},
 	render() {
-		let actions = [];
+		let actions = [
+			{
+				title: "Edit"
+			},
+			{
+				title: "Delete"
+			},
+			{
+				title: "Change the world"
+			}
+		];
 
 		return (
-			<div>
+			<div className="container">
 				<h1>React Popover Control</h1>
-				<div className="post-area">
+				<div className="post-view">
+					This is a popover. Click the little arrow at the bottom right to see more actions.
+					<PopoverControl actions={actions}/>
+				</div>
+
+				<div className="post-view">
+					When the popover doesn't have enough space below it, it will show up above the target.
+					<PopoverControl actions={actions}/>
+				</div>
+
+				<div className="post-view">
+					When the popover doesn't have enough space to the right of it, it will shift itself left and keep a margin to the end of the page.
 					<PopoverControl actions={actions}/>
 				</div>
 			</div>
