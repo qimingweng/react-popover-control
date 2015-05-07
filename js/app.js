d45c3eaf82f84453e26492972ed4b37ec403803a
Qiming Weng <Qiming@This-is-a-MacBook-Pro.local>
2015-05-07 12:22:05 -0400

    Update for 0.0.4

--- js/app.js
+++ js/app.js
@@ -21,17 +21,23 @@ const App = React.createClass({
 				<h1>React Popover Control</h1>
 				<div className="post-view">
 					This is a popover. Click the little arrow at the bottom right to see more actions.
-					<PopoverControl actions={actions}/>
+					<div className="popover-position-wrap">
+						<PopoverControl actions={actions}/>
+					</div>
 				</div>
 
 				<div className="post-view">
 					When the popover doesn't have enough space below it, it will show up above the target.
-					<PopoverControl actions={actions}/>
+					<div className="popover-position-wrap">
+						<PopoverControl actions={actions}/>
+					</div>
 				</div>
 
 				<div className="post-view">
 					When the popover doesn't have enough space to the right of it, it will shift itself left and keep a margin to the end of the page.
-					<PopoverControl actions={actions}/>
+					<div className="popover-position-wrap">
+						<PopoverControl actions={actions}/>
+					</div>
 				</div>
 			</div>
 		)
